6868334b7d175fde9f3fb8eb9f97561b6b2228f8
Naveen <banpan@bbcag.ch>
2021-08-19 16:06:16 +0200

    19.08.21 Kopf Formen done --> next Farben

--- Testing.py
+++ Testing.py
@@ -21,30 +21,33 @@ while run == True:
         print("Als rund bezeichnet man die Form eines Kreises, im Wesentlichen ohne Ecken und Kanten!")     #Definitionen von rund und eckig von Duden
         print("Unter eckig versteht man Formen welche Ecken aufweisen!")
 
-zahlen = False
+zahlene = False
 zahlengrösse = False
+zahlenr = False
 
-"""
-while rund == True:
-    radius = input("Wie gross ist denn der Alienkopf in Zahlen? ")
-    zahlen = radius.isdigit()
-    if zahlen == True:
-        rund = False
-        radius = int(radius)
+
+if rund == True:
+    print("Unsere HD Kameras haben den Radius des besagten Kopfes zwischen 5-150 festgelegt...")
+    radius = input("Wie gross ist denn der Radius des Alienkopfes? ")
+    zahlenr = checkradius(radius)
+
+    while not zahlenr:
+        radius = input("Bitte gib eine Zahl zwischen 5-150 ein! ")
+        zahlenr = checkradius(radius)
+if zahlenr:
         drawcircle_kopf(radius)
-    else:
-        print("Bitte gib eine Zahl ein!")
-        rund = True """
+
+
 
 if eckig:
     ecken = input("Das Alien hat nach ihren Angaben einen Kopf mit Ecken...\nNach unseren Angaben ist die Anzahl Ecken zwischen 3-10.\nWie viele Ecken haben Sie gesehen? ")
-    zahlen = checkeck(ecken)
-    while not zahlen:
+    zahlene = checkeck(ecken)
+    while not zahlene:
         ecken = input("Bitte gib eine natürliche Zahl zwischen 3 und 10 ein! ")
-        zahlen = checkeck(ecken)
+        zahlene = checkeck(ecken)
 
-if zahlen:
-    grösse = input("Mal angenommen das Alein hat wirklich " + ecken +" Ecken...zwischen 1-100, wie gross würden Sie eine Seite einschätzen? ")
+if zahlene:
+    grösse = input("Mal angenommen das Alien hat wirklich " + ecken +" Ecken...zwischen 1-100, wie gross würden Sie eine Seite einschätzen? ")
     zahlengrösse = checkgröss(grösse)
     while not zahlengrösse:
         print("Bitte gib eine natürliche Zahl zwischen 1 und 100 ein!")
--- functions.py
+++ functions.py
@@ -11,7 +11,7 @@ def drawborder(turtrand):
         turtrand.fd(700)
         turtrand.lt(90)
 
-def drawpolygon_kopf(ecken, grösse):
+def drawpolygon_kopf(ecken, grösse, color):
     turtrand = Turtle()
     drawborder(turtrand)
     ecken = int(ecken)
@@ -22,9 +22,13 @@ def drawpolygon_kopf(ecken, grösse):
     t_kopf.up()
     t_kopf.goto(0-grösse/2, 300)
     t_kopf.down()
+    t_kopf.color("black", color)
+    t_kopf.begin_fill()
     for i in range(ecken):
         t_kopf.fd(grösse)
         t_kopf.right(360/ecken)
+    t_kopf.end_fill()
+    t_kopf.ht()
     done()
 
 def checkeck(ecken):
@@ -32,7 +36,7 @@ def checkeck(ecken):
     if not zahl:
         return False
     elif zahl:
-        ecken = float(ecken)
+        ecken = int(ecken)
         if ecken > 10 or ecken < 3:
             return False
         else:
@@ -46,21 +50,58 @@ def checkgröss(grösse):
     if not zahl:
         return False
     elif zahl:
-        grösse = float(grösse)
-        grösse = abs(grösse)
+        grösse = int(grösse)
+
         if grösse > 100:
+            return False
+        elif grösse < 1:
             return False
         else:
             return True
 
-
-
-    done()
-
-def drawcircle_kopf(radius):
+def drawcircle_kopf(radius, color):
+    turtrand = Turtle()
+    drawborder(turtrand)
+    radius = int(radius)
     t_kopf = Turtle()
+    t_kopf.up()
+    t_kopf.goto(0, 300-2*radius)
+    t_kopf.down()
+    t_kopf.color("black", color)
+    t_kopf.begin_fill()
     t_kopf.circle(radius)
+    t_kopf.end_fill()
+    t_kopf.ht()
+
     done()
+
+def checkradius(radius):
+    zahl = radius.isdigit()
+    if not zahl:
+        return zahl
+    else:
+        radius = int(radius)
+        if radius < 5:
+            return False
+        elif radius > 150:
+            return False
+        else:
+            return True
+
+kopf_colors = {
+
+    1 : "crimson",
+    2 : "deeppink",
+    3 : "orange",
+    4 : "lime",
+    5 : "green",
+    6 : "cyan",
+    7 : "navy",
+    8 : "gold",
+    9 : "yellow",
+    10 : "magenta"
+
+}
 
 
 
